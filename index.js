db2ae472fd8ceb0b4b9326eec29cec4f3b51b915
Samuel Woodbridge <s-bridge@live.co.uk>
2017-06-28 15:53:38 +0100

    [NPM] - Changes to the way the package is published on npm

--- index.js
+++
@@ -1,7 +0,0 @@
-import PropForms from './src/PropForms/PropForms';
-import SlickForms from './src/SlickForms/SlickForms';
-
-window.SlickForms = SlickForms;
-window.PropForms = PropForms;
-
-export { PropForms, SlickForms }
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import PropForms from './PropForms/PropForms';
+import SlickForms from './SlickForms/SlickForms';
+
+export { PropForms, SlickForms }
