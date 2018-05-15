dd23e68e827c8cf0d9f91cf8c4e2b51707f99a31
Samuel Woodbridge <s-bridge@live.co.uk>
2018-05-15 16:36:22 +0100

    [Main] - Update initialisation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
 ;(function(window) {
 
-	var forms = document.querySelectorAll('.js-propform');
-	var style = new SlickForms({
-		//exclude: document.querySelectorAll('.no-wrap')
-	});
-
-	for(var i = 0; i < forms.length; i++) {
-		forms[i].addEventListener('error', function(e) {
-			//console.log(e.detail);
-		});
-
-		forms[i].addEventListener('fieldError', function(e) {
-			console.log(e.detail);
-		});
-	}
-
-	window.forms = new PropForms(forms, {
+	var forms = new PropForms(form, {
 		minLengths: {
 			text: 2
 		},
@@ -25,6 +10,9 @@
 		ajax: null
 	});
 
-	console.log(window.forms);
+	form.addEventListener("fieldvalid", function(e) {
+		console.log(e);
+	});
+
 
 })(window);
